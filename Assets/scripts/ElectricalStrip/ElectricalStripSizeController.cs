a3782542941c4115e0f9c4102fff9f1d06058cad
Redstoneweewee <95556046+Redstoneweewee@users.noreply.github.com>
2024-10-28 07:55:44 -0700

    Modified ElectricalStrizSizeController for build

--- Assets/scripts/ElectricalStrip/ElectricalStripSizeController.cs
+++ Assets/scripts/ElectricalStrip/ElectricalStripSizeController.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using Unity.Mathematics;
-using Unity.VisualScripting;
-using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
